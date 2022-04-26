e64982849a38f9cdee34965fda25ef6ebbfbdc0d
SaiKiran <bandarisaikiran8055@gmail.com>
2022-04-26 18:51:35 +0530

    updated

--- new.java
+++ new.java
@@ -1,6 +1,5 @@
 class new{
     public static void main(String[] args){
-        System.out.println("hello world");
-        System.out.println(9);
+        System.out.println("hello world");        System.out.println(9);git 
     }
 }
